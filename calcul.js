b4109f945a275a7f1f92e0c062e726bc83eb6c30
DmiSidorenko <90641562+DmiSidorenko@users.noreply.github.com>
2021-10-15 12:45:37 +0300

    Update calcul.js

--- calcul.js
+++ calcul.js
@@ -1,22 +1,23 @@
-var btn = document.querySelector('#btn');
-var out = document.querySelector('#out');
-var price = document.querySelector('#price');
-var amount = document.querySelector('#amount');
+var btn = document.querySelector("#btn");
+var out = document.querySelector("#out");
+var price = document.querySelector("#price");
+var amount = document.querySelector("#amount");
 var p=price.value;
 var a=amount.value;
 
 function op()
-{  document.querySelector('#price').style.borderColor="red";}
+{  document.querySelector("#price").style.borderColor="red";}
 function oa()
-{  document.querySelector('#amount').style.borderColor="red";}
+{  document.querySelector("#amount").style.borderColor="red";}
 function np()
-{  document.querySelector('#price').style.borderColor="black";}
+{  document.querySelector("#price").style.borderColor="black";}
 function na()
-{  document.querySelector('#amount').style.borderColor="black";}
+{  document.querySelector("#amount").style.borderColor="black";}
 function ob()
-{  document.querySelector('#price').style.borderColor="black";document.querySelector('#amount').style.borderColor="black";}
+{  document.querySelector("#price").style.borderColor="black";
+ document.querySelector("#amount").style.borderColor="black";}
 
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
   console.log("DOM fully loaded and parsed");
  btn.onclick = function() {
   const reg = /-+?|\b0[0-9]+/;
@@ -24,7 +25,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
   const reg2 = /^$/;
   var pro = parseInt(price.value) * parseInt(amount.value);
 if((reg.test(price.value) || reg.test(amount.value))===true ) {
-      alert("Ошибка: Число отрицательное или равно нулю :)")
+      alert("Ошибка: Число отрицательное или равно нулю :)");
    if(reg.test(price.value)===true && reg.test(amount.value)===true) {
      op();oa();
     }
@@ -36,7 +37,7 @@ if((reg.test(price.value) || reg.test(amount.value))===true ) {
     }
 }
 else if((reg1.test(price.value) || reg1.test(amount.value))===true ) {
-  alert(" Ошибка: Вы ввели букву, а не число :)")
+  alert(" Ошибка: Вы ввели букву, а не число :)");
  if(reg1.test(price.value)===true && reg1.test(amount.value)===true) {
   op();oa();
  }
@@ -48,19 +49,19 @@ else if((reg1.test(price.value) || reg1.test(amount.value))===true ) {
   }
 }
 else if((reg2.test(price.value) || reg2.test(amount.value))===true ) {
-  alert(" Ошибка: Введите цифру в соответствующее поле :)")
+  alert(" Ошибка: Введите цифру в соответствующее поле :)");
  if(reg2.test(price.value)===true && reg2.test(amount.value)===true) {
-  oc();ok();
+  op();oa();
  }
  else if(reg2.test(price.value)===true) {
-  oc();nk();
+  op();na();
  }
  else if(reg2.test(amount.value)===true) {
-  on();ok();
+  op();oa();
  }
 }
 else {
  out.innerHTML = pro;
  }
-}
+ }
 });
