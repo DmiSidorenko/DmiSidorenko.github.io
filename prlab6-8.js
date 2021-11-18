ca4d6994cb1f6d87b42d7dfa0525bca9dd322437
DmiSidorenko <90641562+DmiSidorenko@users.noreply.github.com>
2021-11-18 21:20:01 +0300

    Update prlab6-8.js

--- prlab6-8.js
+++ prlab6-8.js
@@ -1,70 +1,52 @@
-var btn = document.querySelector("#btn");
-var out = document.querySelector("#out");
-var price = document.querySelector("#price");
-var amount = document.querySelector("#amount");
-var p=price.value;
-var a=amount.value;
+var btn = document.querySelector('#btn');
+var out = document.querySelector('#out');
+var cena = document.querySelector('#cena');
+var kol = document.querySelector('#kol');
+var c=cena.value;
+var k=kol.value;
 
-function op()
-{  document.querySelector("#price").style.borderColor="red";}
-function oa()
-{  document.querySelector("#amount").style.borderColor="red";}
-function np()
-{  document.querySelector("#price").style.borderColor="black";}
-function na()
-{  document.querySelector("#amount").style.borderColor="black";}
+function oc()
+{  document.querySelector('#cena').style.borderColor="red";}
+function ok()
+{  document.querySelector('#kol').style.borderColor="red";}
+function nc()
+{  document.querySelector('#cena').style.borderColor="black";}
+function nk()
+{  document.querySelector('#kol').style.borderColor="black";}
 function ob()
-{  document.querySelector("#price").style.borderColor="black";
- document.querySelector("#amount").style.borderColor="black";}
+{  document.querySelector('#cena').style.borderColor="black";document.querySelector('#kol').style.borderColor="black";}
 
-window.addEventListener("DOMContentLoaded", function (event) {
+window.addEventListener('DOMContentLoaded', function (event) {
   console.log("DOM fully loaded and parsed");
- btn.onclick = function() {
-  const reg = /-+?|\b0[0-9]+/;
+btn.onclick = function()
+ { const reg = /-+?|\b0[0-9]+/;
   const reg1 = /\D+/;
   const reg2 = /^$/;
-  var pro = parseInt(price.value) * parseInt(amount.value);
-if((reg.test(price.value) || reg.test(amount.value))===true ) {
-      alert("Ошибка: Число отрицательное или равно нулю :)");
-   if(reg.test(price.value)===true && reg.test(amount.value)===true) {
-     op();oa();
-    }
-   else if(reg.test(price.value)===true) {
-    op();na();
-   }
-    else if(reg.test(amount.value)===true) {
-     np();oa();
-    }
+  var pro = parseInt(cena.value) *parseInt(kol.value);
+    if((reg.test(cena.value) ||reg.test(kol.value))===true ) {
+      alert("Ошибка: Число отрицательное или равно нулю")
+    if(reg.test(cena.value)===true && reg.test(kol.value)===true) {oc();ok();}
+   else if(reg.test(cena.value)===true)
+   {oc();nk();}
+    else if(reg.test(kol.value)===true) {nc();ok();}
 }
-else if((reg1.test(price.value) || reg1.test(amount.value))===true ) {
-  alert(" Ошибка: Вы ввели букву, а не число :)");
- if(reg1.test(price.value)===true && reg1.test(amount.value)===true) {
-  op();oa();
- }
- else if(reg1.test(price.value)===true) {
-  op();na();
- }
- else if(reg1.test(amount.value)===true) {
-  op();oa();
-  }
+ else if((reg1.test(cena.value) ||reg1.test(kol.value))===true ) {
+  alert(" Ошибка: Вы ввели букву, а не число!")
+if(reg1.test(cena.value)===true && reg1.test(kol.value)===true) {oc();ok();}
+else if(reg1.test(cena.value)===true)
+{oc();nk();}
+else if(reg1.test(kol.value)===true) {on();ok();}
 }
-else if((reg2.test(price.value) || reg2.test(amount.value))===true ) {
-  alert(" Ошибка: Введите цифру в соответствующее поле :)");
- if(reg2.test(price.value)===true && reg2.test(amount.value)===true) {
-  op();oa();
- }
- else if(reg2.test(price.value)===true) {
-  op();na();
- }
- else if(reg2.test(amount.value)===true) {
-  op();oa();
- }
+else if((reg2.test(cena.value) ||reg2.test(kol.value))===true ) {
+  alert(" Ошибка: Введите цифру в соответствующее поле!")
+if(reg2.test(cena.value)===true && reg2.test(kol.value)===true) {oc();ok();}
+else if(reg2.test(cena.value)===true)
+{oc();nk();}
+else if(reg2.test(kol.value)===true) {on();ok();}
 }
-else {
- out.innerHTML = pro;
- }
- }
-});
+    else {out.innerHTML = pro;ob();} }
+ });
+
 
 // Элементы формы
 const squareInput = document.querySelector('#square-input');
@@ -97,11 +79,11 @@ alert("Повторите ввод данных");
 totalPriceElement.innerText = "0";
 }
 else{
-if(selectnow==1){ 
+if(selectnow==1){
 totalPrice=100;
 totalPrice *= parseInt(squareInput.value);
 }
-else if(selectnow==2){ 
+else if(selectnow==2){
 totalPrice=140;
 for (const radio of radioType) {
 if (radio.checked) {
@@ -215,7 +197,7 @@ $(".popup-form").submit(function() {
     $(".popup-submit").prop('disabled', true);
     $.ajax({
     type: "POST",
-    url: "https://formcarry.com/s/vkxYja3w0Nc",
+    url: "https://formcarry.com/s/nD1zlkZWaqi",
     data: th.serialize(),
     })
     //.done(function() {
